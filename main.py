b3ddf4bce18990ad037abae7c45edeebb0f4a44d
Satyam6024 <sss.sahusatyam2000@gmail.com>
2026-08-06 12:29:12 +0530

    model training added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from EyeDiseaseClassifier import logger
 from EyeDiseaseClassifier.pipeline.stage01_data_ingestion import DataIngestionPipeline
 from EyeDiseaseClassifier.pipeline.stage02_prepare_base_model import PrepareBaseModelPipeline
+from EyeDiseaseClassifier.pipeline.stage03_model_training import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion"
 try:
@@ -23,3 +25,15 @@ try:
 except Exception as e:
     logger.exception(e) 
     raise e
+
+STAGE_NAME = "Model Training"
+try:
+    logger.info(f"\n{'='*20}")
+    logger.info(f"{STAGE_NAME} started ")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f"{STAGE_NAME} completed\n")
+except Exception as e:
+    logger.exception(e) 
+    raise e
+
